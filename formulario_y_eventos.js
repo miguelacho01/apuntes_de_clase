2172ba9c3a3f6f19d54aa092141d88a3568629c7
Miguel_Carvajal <miguelcarvajal7.mc@gmail.com>
2022-09-27 09:39:36 -0500

    login

---
+++ formulario_y_eventos.js
@@ -0,0 +1,46 @@
+//evento reset
+//evento submit
+//input 
+
+
+const boton=document.getElementById('ingresar')
+boton.addEventListener('click',e=>{
+    e.preventDefault()
+    const username=document.getElementById('username').value
+    const password=document.getElementById('password').value
+    
+    if(username==='' ||password ==='')
+    alert('No se indicado un usuario o contraseña')
+
+    
+    if(username==='carlitos' && password==='carlitos123'){
+        alert('BIENVENIDO')
+    } else {
+        document.querySelector('#mensaje').style.display='block'
+        setTimeout(()=>{
+            document.querySelector('#mensaje').style.display='none'
+        },3000)
+        // document.getElementById('mensaje').style.display='block'
+    } 
+    
+})
+
+document.getElementById('password').addEventListener('mouseenter',mostrar)
+
+function mostrar(){
+  
+    document.getElementById('password').type='text'
+}
+
+document.getElementById('password').addEventListener('mouseleave',ocultar)
+
+function ocultar(){
+   document.getElementById('password').type='password'
+}
+
+document.getElementById('rememberme').addEventListener('change',function(z)
+{    
+     if(z.target.checked==true){
+        alert('Desea que recordemos sus datos')
+    }
+})
